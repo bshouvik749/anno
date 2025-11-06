05d335802ba0108cd266eeb9ba22768330baf0eb
bshouvik749 <bshouvik749@gmail.com>
2025-11-06 13:33:51 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://anno-6dyc.onrender.com',
         changeOrigin: true,
       },
     },
